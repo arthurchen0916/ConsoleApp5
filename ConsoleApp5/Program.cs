5611b2f6b5856290a4b031a3f2d81c8705312f0f
Arthur <pooh810916@gmail.com>
2019-05-01 13:29:52 +0800

    test

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class hw1
+    {
+        public void Sort()
+        {
+            Console.Write("Input X : "); //輸入兩數字做加總
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input Y : ");
+            int d = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("X + Y ={0}", c + d);
+            Console.WriteLine();
+        }
+    }
+}
+
