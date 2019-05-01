02e5341b91442ca9936b831294adc1fb2b981f12
Arthur <pooh810916@gmail.com>
2019-05-01 13:30:10 +0800

    test

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -6,17 +6,17 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp5
 {
-    public class hw1
+    class Program
     {
-        public void Sort()
+        static void Main(String[] args)
         {
-            Console.Write("Input X : "); //輸入兩數字做加總
-            int c = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Input Y : ");
-            int d = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("X + Y ={0}", c + d);
-            Console.WriteLine();
+            switch (Console.ReadLine().ToString())
+            {
+                case "7":
+                    var x = new hw7();
+                    x.Sort();
+                    break;
+            }
         }
     }
 }
-
---
+++ ConsoleApp5/hw7.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ConsoleApp5
+{
+    public class hw7
+    {
+        public void Sort()
+        {
+            Random rNnmber = new Random();
+            List<int> lstran = new List<int>();
+
+            for (int i=0; i < 10; i++)
+            {
+                var r = rNnmber.Next(1, 100);
+                Console.WriteLine(r);
+                lstran.Add(r);
+            }
+            Console.WriteLine("---sort---");
+
+            lstran.Sort();
+            foreach (var x in lstran)
+                Console.WriteLine(x);
+
+            Console.ReadKey();
+        }
+    }
+}
